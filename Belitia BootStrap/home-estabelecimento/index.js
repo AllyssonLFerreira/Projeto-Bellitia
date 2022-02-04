f40da806e73523af556dee3e9206803fe4f5e171
AllyssonLFerreira <ferreira.allysson@outlook.com.brgit config --global user.email ferreira.allysson@outlook.com.br>
2022-02-03 22:06:09 -0300

    refinamento

--- Belitia BootStrap/home-estabelecimento/index.js
+++ Belitia BootStrap/home-estabelecimento/index.js
@@ -1,11 +1,46 @@
-const buttonOpenClose = document.getElementById('open-close')
-let verificationBtn = buttonOpenClose.value == "Abrir Estabelecimento"
+const btnOpenClose = document.getElementById(`open-close`)
+const verificationBtn = btnOpenClose.value == "Estabelecimento Aberto"
+const resultado = confirm(`Abrir o estabelecimento agora?`)
 
-buttonOpenClose.addEventListener('click', ()=>{
-    if(verificationBtn = true){
-        buttonOpenClose.innerText=`Estabelecimento Aberto` 
-        buttonOpenClose.style.backgroundColor = `#2bb81e`
-        buttonOpenClose.style.border = `none`
+function openEst(){
+        if(resultado == true){
+            alert(`Estamos Abrindo.`)
+            btnOpenClose.innerText=`Estabelecimento Aberto`, 
+            btnOpenClose.style.backgroundColor = `#2bb81e`,
+            btnOpenClose.style.border = `none`;
+        }else {
+            alert(`Ok! Vamos abrir mais tarde.`)
+        }
+    }
+
+btnOpenClose.addEventListener(`click`, openEst())
+
+
+
+function closeEst(){
     
-    } 
-})
+}
+
+
+
+
+
+
+
+
+/* function openEst(){
+    if (confirm(`Abrir o estabelecimento agora?`)) {
+        alert(`Estamos Abrindo.`)
+        btnOpenClose.innerText=`Estabelecimento Aberto`, 
+        btnOpenClose.style.backgroundColor = `#2bb81e`,
+        btnOpenClose.style.border = `none`;
+    } else {
+        alert(`Ok! Vamos abrir mais tarde.`)
+    }
+}
+
+ function closeEst(){
+        btnOpenClose.removeEventListener(`click`,openEst)
+ }
+
+btnOpenClose.addEventListener(`click`, openEst) */
