94e0608ce150af1f2bc054c14fc193e969d3dd0a
AllyssonLFerreira <ferreira.allysson@outlook.com.brgit config --global user.email ferreira.allysson@outlook.com.br>
2022-03-17 23:44:43 -0300

    configuracao inicial de API

--- bellitia-react/src/Components/Cards/Barra-Lateral.jsx
+++ bellitia-react/src/Components/Cards/Barra-Lateral.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import BtnClose from '../Buttons/BtnClose';
 import BtnOpen from '../Buttons/BtnOpen';
 import LogoutBtn from '../Buttons/Logout';
+
 import './Barra-Lateral.css';
 
 const BarraLateral = () =>{
     return (
         <div className='barra-lateral'>
-            <nav>
-                <ul className='barra-lateral__links'>
-                    <li><a href="">CONTROLE DE AGENDA</a></li>
-                    <li><a href="">CADASTRO DE FUNCIONÁRIO</a></li>
-                    <li><a href="">CADASTRO DE SERVIÇOS</a></li>
-                    <li><a href="">ALTERAÇÃO DE DADOS</a></li>
-                    <li><a href="">CONTROLE FINANCEIRO</a></li>
-                    <li><a href="">PRECISO DE AJUDA</a></li>
-                </ul>
-            </nav>
+                <div className='barra-lateral__links'>
+                    <Link to='#'>CONTROLE DE AGENDA</Link>
+                    <Link to='/novofuncionario'>CADASTRO DE FUNCIONÁRIO</Link>
+                    <Link to='/novoservico'>CADASTRO DE SERVIÇOS</Link>
+                    <Link to='/novoestabelecimento'>ALTERAÇÃO DE DADOS</Link>
+                    <Link to='#'>CONTROLE FINANCEIRO</Link>
+                    <Link to='#'>PRECISO DE AJUDA</Link>
+                </div>
             <div className='btn_prop'>
                 <BtnOpen />
                 <BtnClose />
