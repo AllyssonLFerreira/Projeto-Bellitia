62d0a73d6bc2a053f9adb2fe561a74c0897a80e8
AllyssonLFerreira <ferreira.allysson@outlook.com.brgit config --global user.email ferreira.allysson@outlook.com.br>
2022-05-13 03:36:11 -0300

    insert new css to dropdown-home

--- bellitia-react/src/Components/Buttons/dropDown.jsx
+++ bellitia-react/src/Components/Buttons/dropDown.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import { FaUserAlt } from 'react-icons/fa';
 import './dropDown.css'
 
@@ -11,24 +12,23 @@ const MenuDropDown = () =>{
     
     return (
         <div className="dropDown">
-                <div className="menuDropDown">
-                <button onClick={onClick} className="btn_FaUserAlt"> 
-                <a><i><FaUserAlt/></i></a>
-                </button>
-
-                <nav
-                 ref={DropDownRef}
-                 className={`menuDrop ${isActive ? "active" : "inactive"}`}
-                >  
-                    <ul>
-                        <li className="li1">
-                            <a href="Login">Login Estabelecimento</a>
-                        </li>
-                        <li className="li1">
-                            <a href="userLogin">Login cliente</a>
-                        </li>
-                    </ul>
-                </nav>
+            <div className="menuDropDown">
+              <button onClick={onClick} > 
+                  <a> <i> <FaUserAlt/> </i> </a>
+              </button>
+              <nav 
+                ref={DropDownRef}
+                className={`menuDrop ${isActive ? "active" : "inactive"}`}
+              >  
+                <ul className="access">
+                  <li>
+                      <Link to="Login"> Login Estabelecimento </Link>
+                  </li>
+                  <li>
+                      <Link to="/usuario/login"> Login Cliente </Link>
+                  </li>
+                </ul>
+              </nav>
             </div>
         </div>
     );
