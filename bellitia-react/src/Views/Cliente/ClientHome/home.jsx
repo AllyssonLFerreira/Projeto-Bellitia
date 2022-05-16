dfccca355c005d6a408e09f432ce0f819a4f91fd
Patricia <94497244+Tricia195@users.noreply.github.com>
2022-05-16 01:08:23 -0300

    Update Calendar
     Atualização da página inicial do cliente com calendário e estilização.

--- bellitia-react/src/Views/Cliente/ClientHome/home.jsx
+++ bellitia-react/src/Views/Cliente/ClientHome/home.jsx
@@ -1,6 +1,8 @@
 import Footer from "../../../Components/Cards/Footer";
 import Header from "../../../Components/Cards/Header";
 import BarraLateralCliente from "../../../Components/Cards/NavBarClient";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 import styles from "../../Views.module.css"
 
@@ -8,7 +10,12 @@ export const ClientHome = () => {
     return (
         <div className={styles.view_container}>
             <Header />
+            <div className={styles.agenda}>
             <BarraLateralCliente />
+                <div className={styles.calendario}>
+                <Calendar />
+                </div>
+            </div>
             <Footer />
         </div>
     )
