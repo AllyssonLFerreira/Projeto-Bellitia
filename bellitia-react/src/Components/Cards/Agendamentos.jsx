69f26a2b313faedcb4fdd52b344da50b09331345
Patricia <94497244+Tricia195@users.noreply.github.com>
2022-05-24 20:36:31 -0300

    Atualização tabela
    Atualizado número de colunas da tabela

--- bellitia-react/src/Components/Cards/Agendamentos.jsx
+++ bellitia-react/src/Components/Cards/Agendamentos.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../Cards/cardProps.module.css';
 
-const Agendamentos = () => {
+const Agendamentos = ({ data, column }) => {
     return (
         <div>
             <table className={styles.tabela}>
@@ -9,80 +9,47 @@ const Agendamentos = () => {
                     <tr>
                         <th>Horários</th>
                         <th>Profissional 1</th>
-                        <th>Profissional 2</th>
-                        <th>Profissional 3</th>
-                        <th>Profissional 4</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>09:00</td>
                         <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>10:00</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                         <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>11:00</td>
                         <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>12:00</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                         <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>13:00</td>
                         <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>14:00</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                         <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>15:00</td>
                         <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>16:00</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                         <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>17:00</td>
                         <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                     </tr>
                     <tr>
                         <td>18:00</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
-                        <td>cliente - serviço</td>
                         <td>cliente - serviço</td>
                     </tr>
                 </tbody>
